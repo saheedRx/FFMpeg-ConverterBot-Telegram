99abffbddb4c29f0b269d60737ae84e8332c623a
saheedRx <89293737+saheedRx@users.noreply.github.com>
2022-06-04 11:19:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import re
 from os import mkdir, system
 
 AppId = 2551100  #Replace With Your Own
-AppHash = "e79de98c2b7ac5419f48d2030d60a4ca"  #Replace With Your Own
-BotToken = "5302100774:AAFT0xrULOwh61hgndG75-5Y0CV-ziBBHNU"  #Replace With Your Own
+AppHash = e79de98c2b7ac5419f48d2030d60a4ca
+BotToken = 5302100774:AAFT0xrULOwh61hgndG75-5Y0CV-ziBBHNU
 app = Client("FFMpvdvdvdvegot", api_id=AppId, api_hash=AppHash, bot_token=BotToken)
 
 
